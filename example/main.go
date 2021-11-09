92856d51d4a7480bb329f81a8724d29505b7a626
alessiosavi <alessiosavibtc@gmail.com>
2021-11-09 15:49:25 +0100

    v0.0.20  - Manage ssh keys

--- example/main.go
+++ example/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/alessiosavi/GoGPUtils/helper"
 	stringutils "github.com/alessiosavi/GoGPUtils/string"
 	"github.com/alessiosavi/GoSFTPtoS3"
+	"io/ioutil"
+	"log"
 	"strings"
 )
 
-func main() {
+func Test1() {
 	var sftpConf = &GoSFTPtoS3.SFTPConf{
 		Host:     "test.rebex.net",
 		User:     "demo",
@@ -16,10 +19,11 @@ func main() {
 		Timeout:  5,
 	}
 
-	conn, err := sftpConf.NewConn([]string{"diffie-hellman-group-exchange-sha256"})
+	conn, err := sftpConf.NewConn("diffie-hellman-group-exchange-sha256")
 	if err != nil {
 		panic(err)
 	}
+	defer conn.Close()
 	// Init the default configuration and initialize a new session
 	if err = conn.PutToS3("", "CUSTOM_PREFIX", nil, renameFile); err != nil {
 		panic(err)
@@ -32,4 +36,42 @@ func main() {
 func renameFile(fName string) string {
 	s := strings.Split(fName, "/")
 	return stringutils.JoinSeparator("/", s[1:]...)
+}
+
+func Test2() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+	var sftpConf = &GoSFTPtoS3.SFTPConf{
+		Host:     "localhost",
+		User:     "alessiosavi",
+		Password: "",
+		Port:     22,
+		Bucket:   "bucket-ftp",
+		Timeout:  50,
+		PrivKey:  "",
+	}
+
+	file, err := ioutil.ReadFile("/home/alessiosavi/.ssh/mykey.pem")
+	if err != nil {
+		return
+	}
+	sftpConf.PrivKey = string(file)
+
+	conn, err := sftpConf.NewConn()
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+	list, err := conn.List("/tmp")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(helper.MarshalIndent(list))
+
+}
+
+func main() {
+	Test1()
+	Test2()
 }
